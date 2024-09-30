e8e2d5a601bbb46a90f4c8da342a3631b8c103b5
Croissanthology <croissanthology@gmail.com>
2024-10-01 01:04:05 +0200

    Update javaventuraayayay.js

--- javascript/javaventuraayayay.js
+++ javascript/javaventuraayayay.js
@@ -1,28 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.mobile-header');
     const progressBar = document.querySelector('.progress-bar');
-    const headerTitle = document.querySelector('.mobile-header-title');
     const showHeaderThreshold = 100;
-    const isMobile = () => window.innerWidth < 768;
-
-    // Set the header title
-    headerTitle.textContent = document.title;
 
     function updateProgressBar() {
-        const scrollPosition = window.pageYOffset;
-        const pageHeight = document.documentElement.scrollHeight - window.innerHeight;
-        const scrollPercentage = (scrollPosition / pageHeight) * 100;
-
+        const scrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
         progressBar.style.width = `${scrollPercentage}%`;
-
-        header.style.transform = scrollPosition > showHeaderThreshold ? 'translateY(0)' : 'translateY(-100%)';
-
-     
+        header.style.transform = `translateY(${window.scrollY > showHeaderThreshold ? '0' : '-100%'})`;
+    }
 
     window.addEventListener('scroll', updateProgressBar);
-    window.addEventListener('resize', updateProgressBar);
-
-    // Initial update
-    updateProgressBar();
+    updateProgressBar(); // Initial call
 });
-
